ca2918cdc03bda3bb070924f94251b350efe65d9
inuse918 <s2018s34@e-mirim.hs.kr>
2020-04-28 14:22:14 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,25 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "유" + "채" + "린";
+            Console.Write("Hello C#");
+            int add = 2;
+            int Alpha;
+
+            Console.WriteLine("Hello World");
+            Console.Write("Hello World");
+
+            Console.Write(52);
+            Console.Write(7 % 3);
+            Console.Write('A');
+            Console.Write("A");
+
+            Console.WriteLine("김도영\t노래너무잘불렁");
+            Console.WriteLine("이상연\n셀카올려");
+            Console.WriteLine("\"\"\"");
+
+            Console.WriteLine("이" + "상" + "연");
+
         }
     }
 }
